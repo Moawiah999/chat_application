b8f8119c036b3934bdc68cdc90d8e2726dba69a4
moawiah <muawiyah454@gmail.com>
2026-03-09 19:41:21 +0300

    login screen

--- lib/screen/auth/login_screen.dart
+++ lib/screen/auth/login_screen.dart
@@ -1,3 +1,5 @@
+import 'package:chatapp/screen/auth/registration_screen.dart';
+import 'package:chatapp/widget/auth_text_field.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -8,8 +10,116 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Color(0xFFF0F5F8),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            alignment: Alignment.center,
+
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 80),
+              child: Card(
+                shadowColor: Colors.white,
+                elevation: 10,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(40),
+                ),
+                color: Color(0xffF0F3F7),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      SizedBox(height: 10),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.chat_bubble_outline_sharp,
+                            color: Color(0xFF00BFFF),
+                            size: 30,
+                          ),
+                          SizedBox(width: 5),
+                          Text(
+                            'Chating',
+                            style: TextStyle(
+                              fontWeight: FontWeight.w500,
+                              fontSize: 22,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 100),
+                      AuthTextField(
+                        controller: emailController,
+                        hintText: 'Email',
+                        icons: Icon(Icons.person, color: Color(0xFF00BFFF)),
+                      ),
+                      SizedBox(height: 20),
+                      AuthTextField(
+                        controller: passwordController,
+                        hintText: 'Password',
+                        icons: Icon(Icons.lock, color: Color(0xFF00BFFF)),
+                      ),
+                      SizedBox(height: 30),
+                      SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Color(0xff0080E3),
+                            padding: EdgeInsets.symmetric(vertical: 15),
+                          ),
+                          onPressed: () {
+                            print(emailController);
+                            print(passwordController);
+                          },
+                          child: Text(
+                            'Login',
+                            style: TextStyle(color: Colors.white, fontSize: 16),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Text(
+                        'Forgot password',
+                        style: TextStyle(
+                          color: const Color.fromARGB(255, 27, 152, 253),
+                        ),
+                      ),
+                      SizedBox(height: 15),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) {
+                                return RegistrationScreen();
+                              },
+                            ),
+                          );
+                        },
+                        child: Text(
+                          'Create a new account',
+                          style: TextStyle(
+                            color: const Color.fromARGB(255, 27, 152, 253),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 15),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
