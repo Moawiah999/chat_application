48324f3aa309cfc07c74c0450ab686b8fecf6871
moawiah <muawiyah454@gmail.com>
2026-03-09 21:32:59 +0300

    add screen chat and friend profile

--- lib/screen/home/home_screen.dart
+++ lib/screen/home/home_screen.dart
@@ -1,3 +1,6 @@
+import 'package:chatapp/screen/home/chat_screen.dart';
+import 'package:chatapp/screen/home/friends_screen.dart';
+import 'package:chatapp/screen/home/profile_screen.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,12 +11,30 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+  final List<Widget> _screens = [
+    ChatScreen(),
+    FriendsScreen(),
+    ProfileScreen(),
+  ];
+  final List<String> _titles = ['Chats', 'Friends', 'Profile'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(backgroundColor: Color(0xFF00BFFF)),
+      body: _screens[_selectedIndex],
+      appBar: AppBar(
+        backgroundColor: Color(0xFF00BFFF),
+        title: Text(_titles[_selectedIndex]),
+      ),
       backgroundColor: Color(0xFFF0F5F8),
       bottomNavigationBar: NavigationBar(
+        selectedIndex: _selectedIndex,
+        onDestinationSelected: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
         destinations: [
           NavigationDestination(
             icon: Icon(
