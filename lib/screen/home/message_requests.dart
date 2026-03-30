b857466a553e675ca060012fe7247521e6d04ecf
moawiah <muawiyah454@gmail.com>
2026-03-30 19:32:17 +0300

    message screen

---
+++ lib/screen/home/message_requests.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MessageRequests extends StatelessWidget {
+  const MessageRequests({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text('Message Requests')));
+  }
+}
